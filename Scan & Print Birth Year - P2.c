52c7a30618cfa1d8dca01a65334916d15914b64c
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-10-30 19:34:19 +0530

    Scan & Print Birth Year

---
+++ Scan & Print Birth Year - P2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    printf("Enter your birth year : ");
+    scanf ("%d", &year);
+    printf("Your birth year is : %d", year);
+
+    return 0;
+}
