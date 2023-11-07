8a8d4006c22064977826c08ad3297f2f8b704ec7
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-07 19:45:31 +0530

    Check whether a no. entered by user is odd or even ?

---
+++ No. is odd or even - P6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int num;
+    printf ("Ener a number : ");
+    scanf ("%d", &num);
+    if (num%2==0){
+        printf("%d is a even number!", num);
+    }
+    else {
+        printf("%d is a odd number!", num);
+    }
+
+    return 0;
+}
