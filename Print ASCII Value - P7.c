58f025ba0a74a48a97da63c79b10d0528f3fa035
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-07 19:49:21 +0530

    Print ASCII Value of a character entered by user

---
+++ Print ASCII Value - P7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    char ch;
+    printf ("Ener a character to know its ASCII value : ");
+    scanf ("%c", &ch);
+    printf("The ASCII value of %c is %d",ch,ch);
+
+    return 0;
+}
