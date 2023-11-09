f1f269a1536bc297f6634dc7f84c22985b6bac2c
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-09 19:17:24 +0530

    Implements all the relational operators (<, >, <=, >=, !=, ==)

---
+++ Implements all the relational operators: - P23.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+
+    printf("%d\n", 3>4);
+    printf("%d\n", 5<4);
+    printf("%d\n", 6<=4);
+    printf("%d\n", 6>=4);
+    printf("%d\n", 6!=4);
+    printf("%d\n", 7==7);
+    
+
+    return 0;
+}
