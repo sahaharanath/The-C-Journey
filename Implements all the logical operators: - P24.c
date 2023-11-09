efdc45530e133c2b8fe7b7ea112dc964fd5222f4
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-09 19:24:31 +0530

    Implement all the logical operators: ( & , || , ! )

---
+++ Implements all the logical operators: - P24.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+
+    printf("%d\n", 3>4 && 4<5); //AND Operator
+    printf("%d\n", 5<4 || 6>9); //OR Operator
+    printf("%d\n", !(5<2)); //NOT Operator
+    
+    
+    return 0;
+}
