2bdbbe782b0cbf4828a335bf260086666ab6b2df
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-24 16:47:20 +0530

    Demonstrates a linker error

---
+++ P30-Linker Error Example .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Linker error: Undefined function
+void undefinedFunction();
+
+int main() {
+    // Uncomment the line below to trigger a linker error
+    // undefinedFunction();
+
+    return 0;
+}
