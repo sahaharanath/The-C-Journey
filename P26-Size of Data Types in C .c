cccd741e0e0ddc7138af305e9890d2beb818da65
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-24 16:26:44 +0530

    Print the size of various data types using the sizeof operator

---
+++ P26-Size of Data Types in C .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Print the size of various data types using sizeof operator
+    printf("Size of char: %d bytes\n", sizeof(char));
+    printf("Size of short: %d bytes\n", sizeof(short));
+    printf("Size of int: %d bytes\n", sizeof(int));
+    printf("Size of long: %d bytes\n", sizeof(long));
+    printf("Size of long long: %d bytes\n", sizeof(long long));
+    printf("Size of float: %d bytes\n", sizeof(float));
+    printf("Size of double: %d bytes\n", sizeof(double));
+    printf("Size of long double: %d bytes\n", sizeof(long double));
+
+    return 0;
+}
