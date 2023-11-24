25903f5f1c5fc41096e7542dc05b7483fc7dc1be
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-24 16:55:33 +0530

    Print various escape characters in C, including new line, horizontal tab, carriage return, audible bell, single quotation, double quotation, question mark sequence, backslash, form feed, and vertical tab.

---
+++ P31-Escape Sequences .c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    // \n: new line
+    printf("Hello\nWorld\n");
+
+    // \t: horizontal tab
+    printf("Hello\tWorld\n");
+
+    // \r: carriage return
+    printf("Hello\rWorld\n");
+
+    // \a: audible bell
+    printf("Hello\aWorld\n");
+
+    // \': print single quotation
+    printf("Hello \'World\'\n");
+
+    // \": print double quotation
+    printf("Hello \"World\"\n");
+
+    // \?: question mark sequence
+    printf("Hello\?World\n");
+
+    // \\: backslash
+    printf("Hello\\World\n");
+
+    // \f: form feed
+    printf("Hello\fWorld\n");
+
+    // \v: vertical tab
+    printf("Hello\vWorld\n");
+
+    return 0;
+}
