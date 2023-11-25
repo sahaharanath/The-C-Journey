155b29a247c0efea92cb8878f5a3fab75b45d6aa
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-25 20:56:21 +0530

    Prints a specified character multiple times based on user input using a for loop

---
+++ P56-Print Character Multiple Times (With taking input of both character and number) .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+    
+    char ch;
+    printf("Enter a character : ");
+    scanf ("%c", &ch);
+
+    int n;
+    printf("Enter how many times do you want to print %c: ",ch);
+    scanf ("%d", &n);
+
+    for (int i=1;i<=n;i++){
+        printf("%c\n",ch);
+    }
+    
+    
+    return 0;
+}
