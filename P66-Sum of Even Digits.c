11b51a994c4a2376dc8a3fe4c39d40b00112ef14
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2023-11-25 22:49:44 +0530

    Calculates the sum of even digits in a given number using a while loop

---
+++ P66-Sum of Even Digits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num, last_digit, sum = 0;
+
+    printf("Enter a number to find the sum of its even digits only: ");
+    scanf("%d", &num);
+
+    // Store the original value of num in backup
+    int backup = num;
+
+    // Calculate the sum of even digits
+    while (num != 0) {
+        last_digit = num % 10;
+
+        if (last_digit % 2 == 0) {
+            sum = sum + last_digit;
+        }
+
+        num = num / 10;
+    }
+
+    printf("The sum of even digits of %d is: %d", backup, sum);
+
+    return 0;
+}
